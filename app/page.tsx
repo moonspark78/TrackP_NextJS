8253a6887df96d2fb2ad94a3a0013b75c5d1c66e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-24 17:20:12 +0100

    📈 stil working navbar  📈

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='overflow-hidden'>
+    <div className='overflow-hidden h-[2000px]'>
       <Home/>
     </div>
   )
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,11 +1,32 @@
+"use client";
+
 import { navLinks } from '@/constants/Constants'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HiBars3BottomRight } from 'react-icons/hi2'
 
 const Nav = () => {
+
+    const [navBg, SetNavBg] = useState(false);
+
+    useEffect(() =>{
+        const handler = () =>{
+            if(window.scrollY >= 90) SetNavBg(true)
+            if(window.scrollY < 90) SetNavBg(false)
+        }
+
+        window.addEventListener("scroll",handler)
+
+        return () => {
+            window.addEventListener("scroll",handler)
+        }
+    },[])
+
+
+
+
   return (
-    <div className='w-full transition-all duration-200 h-[12vh] z-[1000]'>
+    <div className={` fixed ${navBg ? "bg-white shadow-md " : "fixed"} w-full transition-all duration-200 h-[12vh] z-[1000]`}>
         <div className='flex items-center h-full justify-between w-[90%] lg:w-[80%] mx-auto'>
             {/* Logo */}
             <h1 className='text-xl md:text-2xl font-bold'>Track. <span className='text-3xl md:text-4xl text-blue-600'>P</span></h1>
