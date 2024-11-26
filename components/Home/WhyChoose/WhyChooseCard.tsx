45250667d05b8f33cc28512fa680a82f888eb3f3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-26 17:20:01 +0100

    📈 working on  WhyChoose section  📈

--- components/Home/WhyChoose/WhyChooseCard.tsx
+++ components/Home/WhyChoose/WhyChooseCard.tsx
@@ -18,7 +18,8 @@ const WhyChooseCard = ({image,title,linkText}:Props) => {
             className='object-contain mx-auto'
         />
         <h1 className='text-center text-lg mt-5 mb-5 font-semibold text-gray-800'>{title}</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, eum!</p>
+        <p className='text-gray-600 font-medium text-center text-sm mb-7'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, eum!</p>
+        <p className='text-center font-semibold text-blue-500 hover:text-blue-700 transition-all duration-200 cursor-pointer'>{linkText}</p>
     </div>
   )
 }
