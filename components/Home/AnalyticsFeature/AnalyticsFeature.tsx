2587b039644cf9430b18fcc5b8b56933c307796b
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-27 22:39:35 +0100

    📈 CREATE analytic  section  📈

---
+++ components/Home/AnalyticsFeature/AnalyticsFeature.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AnalyticsFeature = () => {
+  return (
+    <div>AnalyticsFeature</div>
+  )
+}
+
+export default AnalyticsFeature
--- components/Home/WhyChoose/WhyChooseCard.tsx
+++ components/Home/WhyChoose/WhyChooseCard.tsx
@@ -1,27 +1,33 @@
-import Image from 'next/image';
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-type Props ={
-    image: string;
-    title: string;
-    linkText: string;
-}
+type Props = {
+  image: string;
+  title: string;
+  linkText: string;
+};
 
-const WhyChooseCard = ({image,title,linkText}:Props) => {
+const WhyChooseCard = ({ image, title, linkText }: Props) => {
   return (
     <div>
-        <Image
-            src={image}
-            alt={title}
-            width={80}
-            height={80}
-            className='object-contain mx-auto'
-        />
-        <h1 className='text-center text-lg mt-5 mb-5 font-semibold text-gray-800'>{title}</h1>
-        <p className='text-gray-600 font-medium text-center text-sm mb-7'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, eum!</p>
-        <p className='text-center font-semibold text-blue-500 hover:text-blue-700 transition-all duration-200 cursor-pointer'>{linkText}</p>
+      <Image
+        src={image}
+        alt={title}
+        width={80}
+        height={80}
+        className="object-contain mx-auto"
+      />
+      <h1 className="text-center text-lg mt-5 mb-5 font-semibold text-gray-800">
+        {title}
+      </h1>
+      <p className="text-gray-600 font-medium text-center text-sm mb-7">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, eum!
+      </p>
+      <p className="text-center font-semibold text-blue-500 hover:text-blue-700 transition-all duration-200 cursor-pointer">
+        {linkText} &#8594;
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default WhyChooseCard
+export default WhyChooseCard;
