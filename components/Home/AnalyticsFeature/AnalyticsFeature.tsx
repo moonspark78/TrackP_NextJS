160fc7fbf2a25e4c5782db6cdec9787cdfb301c3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-27 23:32:37 +0100

    📈 adding image in  analytics  section  📈

--- components/Home/AnalyticsFeature/AnalyticsFeature.tsx
+++ components/Home/AnalyticsFeature/AnalyticsFeature.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 const AnalyticsFeature = () => {
   return (
@@ -6,6 +7,17 @@ const AnalyticsFeature = () => {
         {/* Define Grid */}
         <div className='w-[95%] sm:w-[80%] mx-auto items-center grid grid-cols-1 lg:grid-cols-2 gap-10'>
             {/* Image Content */}
+            <div>
+                <Image
+                    src="/images/a.jpg"
+                    alt="images"
+                    width={500}
+                    height={500}
+                    className="object-contain"
+                />
+            </div>
+            {/* Text content */}
+            <div></div>
         </div>
     </div>
   )
