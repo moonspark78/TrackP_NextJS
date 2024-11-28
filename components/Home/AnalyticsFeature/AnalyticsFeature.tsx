aaa2dc29f9d03a329c78b459c511027a3ec691fb
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-28 22:00:04 +0100

    📈 still working on the  analytics  sections  📈

--- components/Home/AnalyticsFeature/AnalyticsFeature.tsx
+++ components/Home/AnalyticsFeature/AnalyticsFeature.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import { FaCheckCircle } from 'react-icons/fa'
 
 const AnalyticsFeature = () => {
   return (
@@ -21,14 +22,20 @@ const AnalyticsFeature = () => {
               <h1 className='text-base font-semibold text-orange-500'>
                 Audiance tracking and insight
               </h1>
-              <h1 className='mt-4 text-xl sm:text-2xl md:text-3xl font-bold text-gray-900'>
+              <h1 className='mt-4 text-xl sm:text-2xl md:text-4xl font-bold text-gray-900'>
                 Powerful analytics tools that put you in control and are fully customizable.
               </h1>
-              <p>
+              <p className='mt-4 text-gray-600 text-lg font-medium leading-[2rem]'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Iure, dolor? Delectus autem libero cum qui beatae architecto cupiditate, ab ratione.
                  Sint possimus temporibus natus quaerat.
               </p>
+              <ul className='mt-7 space-y-2 text-gray-800'>
+                <li className='flex items-center font-semibold'>
+                  <FaCheckCircle className='text-green-500 mr-2'/>
+                  Chat prompt module supported
+                </li>
+              </ul>
             </div>
         </div>
     </div>
