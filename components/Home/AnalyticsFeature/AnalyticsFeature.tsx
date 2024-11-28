603916a054f2d6e33d6c8b4e5b68d7220325fc94
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-28 21:54:38 +0100

    📈 still working on the  analytics  section  📈

--- components/Home/AnalyticsFeature/AnalyticsFeature.tsx
+++ components/Home/AnalyticsFeature/AnalyticsFeature.tsx
@@ -17,7 +17,19 @@ const AnalyticsFeature = () => {
                 />
             </div>
             {/* Text content */}
-            <div></div>
+            <div className='p-6'>
+              <h1 className='text-base font-semibold text-orange-500'>
+                Audiance tracking and insight
+              </h1>
+              <h1 className='mt-4 text-xl sm:text-2xl md:text-3xl font-bold text-gray-900'>
+                Powerful analytics tools that put you in control and are fully customizable.
+              </h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Iure, dolor? Delectus autem libero cum qui beatae architecto cupiditate, ab ratione.
+                 Sint possimus temporibus natus quaerat.
+              </p>
+            </div>
         </div>
     </div>
   )
