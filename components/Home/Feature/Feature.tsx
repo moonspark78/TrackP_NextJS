60e0b0710d1e2627b54844003578a5699cb5b61d
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-30 14:50:30 +0100

    📈working  feature  sections  📈

--- components/Home/Feature/Feature.tsx
+++ components/Home/Feature/Feature.tsx
@@ -1,6 +1,29 @@
 import React from 'react'
+import { FaCog, FaDesktop, FaLaptop, FaLayerGroup, FaMobileAlt } from 'react-icons/fa'
 
 const Feature = () => {
+    const features = [
+        {
+            icon: <FaLayerGroup className='text-red-500'/>,
+            text: "50+ Unique Design Block",
+        },
+        {
+            icon: <FaLaptop className='text-blue-500'/>,
+            text: "Multiple Layouts",
+        },
+        {
+            icon: <FaMobileAlt className='text-yellow-500'/>,
+            text: "Mobile First Design",
+        },
+        {
+            icon: <FaDesktop className='text-purple-500'/>,
+            text: "Fully responsive",
+        },
+        {
+            icon: <FaCog className='text-teal-500'/>,
+            text: "Customizable Features",
+        },
+    ]
   return (
     <div>Feature</div>
   )
