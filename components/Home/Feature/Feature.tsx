ba4a7b2b6000319938a205d590b3a8bf7f526433
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-30 15:23:03 +0100

    📈working  feature  sections  📈

--- components/Home/Feature/Feature.tsx
+++ components/Home/Feature/Feature.tsx
@@ -41,7 +41,24 @@ const Feature = () => {
         },
     ]
   return (
-    <div>Feature</div>
+    <div className='bg-pink-50 pt-20 pb-20'>
+        <div className='w-[80%] mx-auto text-center'>
+            <h1 className='mt-6 text-2xl md:text-3xl capitalize font-bold text-center'>
+                Its  Everything you will ever need 
+            </h1>
+            <div className='grid mt-16 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {features.map((feature, index) => (
+                    <div key={index} className='flex items-center justify-center p-4 bg-white rounded-lg shadow-md space-x-3'>
+                            <div className='text-3xl w-14 h-14'>
+                                <span>{feature.icon}</span>
+                            </div>
+                            <span>{feature.text}</span>
+                            
+                    </div>
+                ))}
+            </div>
+        </div>
+    </div>
   )
 }
 
